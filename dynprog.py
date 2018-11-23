480511e81cf66bb968207af2f84041a90b1a4dce
Marc-André Reichelt <marc.reichelt94@gmail.com>
2018-11-23 19:18:00 +0100

    added .py

---
+++ dynprog.py
@@ -0,0 +1,13 @@
+import gym
+import quanser_robots
+import math
+import numpy as np
+
+
+
+# discretize state and action spaces
+# gather data for regression using exploration policy
+# learn dynamics function
+# learn reward function
+# policy iteration / value iteration
+# plot results
