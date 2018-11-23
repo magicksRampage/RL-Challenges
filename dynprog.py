e2ede4d5d57fd89555b74b7a56b9a80c244c7b6f
Marc-André Reichelt <marc.reichelt94@gmail.com>
2018-11-23 21:20:15 +0100

    added uniform discretization

--- dynprog.py
+++ dynprog.py
@@ -1,9 +1,34 @@
 import gym
 import quanser_robots
-import math
 import numpy as np
+import random as rnd
+
+def makeEnv(whichEnv):
+    if whichEnv:
+       return gym.make('Qube-v0')
+    else:
+       return gym.make('Pendulum-v0')
+
+def discretizeSpace(space, grain):
+    shape = space.shape
+    highs = space.high
+    lows = space.low
+    discSpace = np.ones([shape[0],grain])
+    for i in range(shape[0]):
+        step = (highs[i] - lows[i]) / grain
+        for j in range(grain):
+            discSpace[i][j] = lows[i] + j * step
+    return discSpace
 
 
+def main():
+    # false for pendulum, true for qube
+    qube = False
+    env = makeEnv(qube)
+    discActions = discretizeSpace(env.action_space, 10)
+    discStates = discretizeSpace(env.observation_space, 10)
+    print(discActions)
+    print(discStates)
 
 # discretize state and action spaces
 # gather data for regression using exploration policy
